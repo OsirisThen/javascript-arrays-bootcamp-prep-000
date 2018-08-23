f33c3676c44ee62dadf1385a9f6c14529a2011a1
Osiris Then <osiriscuevas@hotmail.com>
2018-08-23 23:51:52 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,3 +25,8 @@ function destructivelyAddElementToEndOfArray(array, element){
   array.push(element);
   return array;
 }
+
+function accessElementInArray(array, index){
+  var first = array[index]
+  return first;
+}
