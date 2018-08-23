5a06f6f79bf6b54f231dfd121589eaf2d29f66a6
Osiris Then <osiriscuevas@hotmail.com>
2018-08-23 22:45:39 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,6 +7,6 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(array, element){
 
   array.unshift(element);
-  array.shift();
   return array;
+  //array.shift();
 }
