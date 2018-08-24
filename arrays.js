47ee7eaff63d9ea48a650d4a9038d60c22d9558d
Osiris Then <osiriscuevas@hotmail.com>
2018-08-24 00:01:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -37,6 +37,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array, element){
 }
 
 function removeElementFromBeginningOfArray(array){
-   //array.slice(1);
-   return array;
+   let newArray= array.slice(1);
+   return newArray;
 }
